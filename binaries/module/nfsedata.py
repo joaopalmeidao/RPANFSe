24c9618f8a795fdecce46bfe7407f27a72a68bf0
João Pedro Almeida Oliveira <96277609+joaopalmeidao@users.noreply.github.com>
2022-09-08 16:04:38 -0300

    Update nfsedata.py

--- binaries/module/nfsedata.py
+++ binaries/module/nfsedata.py
@@ -75,49 +75,49 @@ NA TABELA a_nfse_data DO BANCO DE DADOS C:\\RPA NFSe\\{DataBase}.bd
         cont += 1
         time = int(final_each - start)
 
-#         except:
-#             print(f"""///////////////////////////////////////////////////////////////
-# Erro ao ler {i}
+        except:
+            print(f"""///////////////////////////////////////////////////////////////
+Erro ao ler {i}
+///////////////////////////////////////////////////////////////""")
+        if SAVE_NFSE_DB == True:
+            try:
+                cursor.execute(
+                    f"""
+                    INSERT INTO xml_nfse VALUES(
+                    '{nota_xml[0]}',
+                    '{nota_xml[1]}',
+                    '{nota_xml[2]}',
+                    '{nota_xml[3]}',
+                    '{nota_xml[4]}',
+                    '{nota_xml[5]}',
+                    '{nota_xml[6]}',
+                    '{nota_xml[7]}',
+                    '{nota_xml[8]}',
+                    '{nota_xml[9]}',
+                    '{nota_xml[10]}',
+                    '{nota_xml[11]}',
+                    '{nota_xml[12]}',
+                    '{nota_xml[13]}',
+                    '{nota_xml[14]}',
+                    '{nota_xml[15]}',
+                    '{nota_xml[16]}',
+                    '{nota_xml[17]}',
+                    '{nota_xml[18]}',
+                    '{nota_xml[19]}',
+                    '{nota_xml[20]}',
+                    '{nota_xml[21]}',
+                    '{nota_xml[22]}',
+                    '{nota_xml[23]}',
+                    '{nota_xml[24]}'
+                    )
+                    """
+                )
+                db.commit()
+            except:
+                pass
+        else:
+            pass
+    db.commit()
+    print(f"""\n///////////////////////////////////////////////////////////////
+{cont} NOTA(S) COLETADAS
 # ///////////////////////////////////////////////////////////////""")
-#         if SAVE_NFSE_DB == True:
-#             try:
-#                 cursor.execute(
-#                     f"""
-#                     INSERT INTO xml_nfse VALUES(
-#                     '{nota_xml[0]}',
-#                     '{nota_xml[1]}',
-#                     '{nota_xml[2]}',
-#                     '{nota_xml[3]}',
-#                     '{nota_xml[4]}',
-#                     '{nota_xml[5]}',
-#                     '{nota_xml[6]}',
-#                     '{nota_xml[7]}',
-#                     '{nota_xml[8]}',
-#                     '{nota_xml[9]}',
-#                     '{nota_xml[10]}',
-#                     '{nota_xml[11]}',
-#                     '{nota_xml[12]}',
-#                     '{nota_xml[13]}',
-#                     '{nota_xml[14]}',
-#                     '{nota_xml[15]}',
-#                     '{nota_xml[16]}',
-#                     '{nota_xml[17]}',
-#                     '{nota_xml[18]}',
-#                     '{nota_xml[19]}',
-#                     '{nota_xml[20]}',
-#                     '{nota_xml[21]}',
-#                     '{nota_xml[22]}',
-#                     '{nota_xml[23]}',
-#                     '{nota_xml[24]}'
-#                     )
-#                     """
-#                 )
-#                 db.commit()
-#             except:
-#                 pass
-#         else:
-#             pass
-#     db.commit()
-#     print(f"""\n///////////////////////////////////////////////////////////////
-# {cont} NOTA(S) COLETADAS
-# # ///////////////////////////////////////////////////////////////""")
